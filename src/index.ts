1c4cf52147fb95bd3f24b608f9d9e7fa3db43d89
Viraj Shah <shahvirajd@gmail.com>
2022-02-09 18:39:04 -0500

    Added some dummy code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import * as express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (_, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}`);
+});
