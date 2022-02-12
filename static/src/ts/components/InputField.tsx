e5012271dc72b05c6430be708e8c8f53f9895372
Viraj Shah <shahvirajd@gmail.com>
2022-02-11 20:31:00 -0500

    Improved the password validator

--- static/src/ts/components/InputField.tsx
+++ static/src/ts/components/InputField.tsx
@@ -14,7 +14,6 @@ export interface InputFieldState {
     value: string;
     isValid: string;
     type: string;
-    buttonState: boolean;
 }
 
 export class InputFieldValidationError extends Error {
@@ -31,7 +30,6 @@ export default abstract class InputField extends React.Component<InputFieldProps
             value: props.value ?? '',
             isValid: 'ic',
             type: props.type ?? 'text',
-            buttonState: false,
         };
     }
 
@@ -98,21 +96,57 @@ export class EmailFieldComponent extends TextFieldComponent {
     override validate(value: string) {
         if (value.indexOf(' ') >= 0)
             throw new InputFieldValidationError('An email should not have any spaces in it');
-
         if (value.length == 0) return false;
-
         if (value.indexOf('@') < 0) return false;
-
         return true;
     }
 }
 
 export class PasswordFieldComponent extends InputField {
+    private static readonly allowedChars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()<>?;:'[]{}-=_+|";
+
     constructor(props: InputFieldProps) {
         super(props);
     }
 
-    override validate() {
-        return false;
+    componentDidMount() {
+        this.setState({ type: 'password' });
+    }
+
+    override validate(value: string) {
+        if (value.length < 8) return false;
+        if (value.length > 60)
+            throw new InputFieldValidationError(
+                `The provided password length is too long (${value.length})`
+            );
+        let uppercase = 0;
+        let lowercase = 0;
+        let symbols = 0;
+
+        const aIndex = PasswordFieldComponent.allowedChars.indexOf('a');
+        const AIndex = PasswordFieldComponent.allowedChars.indexOf('A');
+        const zIndex = PasswordFieldComponent.allowedChars.indexOf('z');
+        const ZIndex = PasswordFieldComponent.allowedChars.indexOf('Z');
+
+        for (const c of value) {
+            const cIndex = PasswordFieldComponent.allowedChars.indexOf(c);
+            if (cIndex < 0) {
+                throw new InputFieldValidationError(
+                    `Password cannot contain invalid character (${c}).`
+                );
+            } else if (cIndex >= aIndex && cIndex <= zIndex) {
+                lowercase++;
+            } else if (cIndex >= AIndex && cIndex <= ZIndex) {
+                uppercase++;
+            } else {
+                symbols++;
+            }
+        }
+
+        if (lowercase < 1) return false;
+        if (uppercase < 1) return false;
+        if (symbols < 1) return false;
+        return true;
     }
 }
