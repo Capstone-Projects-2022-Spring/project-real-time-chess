3fe6f1402418d9727a423408c9e2702aab8f5fb0
Viraj Shah <shahvirajd@gmail.com>
2022-02-21 16:52:43 -0500

    Updated router for creating a user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as express from 'express';
 import * as path from 'path';
+import DatabaseConnector from './dao/DatabaseConnector';
+import UserDAO from './dao/UserDAO';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+
+DatabaseConnector.open();
 
 app.use(express.static(path.resolve('./static/public')));
 app.use(bodyParser({ extended: true }));
@@ -14,8 +18,20 @@ app.get('/', (_, res) => {
     res.render('index');
 });
 
-app.post('/api/users/create', (_, res) => {
-    res.send();
+app.post('/api/user/create', (req, res) => {
+    const dao = new UserDAO();
+    dao.createUser(req.body)
+        .then(() =>
+            res.send({
+                success: true,
+            })
+        )
+        .catch(error =>
+            res.send({
+                success: false,
+                error,
+            })
+        );
 });
 
 app.listen(PORT, () => {
