4f9221c65453a6c6941797c58e97b586fdd2bdf7
Viraj Shah <shahvirajd@gmail.com>
2022-03-11 16:24:19 -0500

    Added popup container

--- static/src/ts/components/Popup.tsx
+++ static/src/ts/components/Popup.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import ButtonComponent from './ButtonComponent';
 
 export interface PopupProps {
@@ -7,7 +8,7 @@ export interface PopupProps {
     buttons?: [ButtonComponent];
 }
 
-export default class Popup extends React.Component<PopupProps> {
+export default class PopupComponent extends React.Component<PopupProps> {
     render() {
         return (
             <div className="popup">
@@ -18,5 +19,11 @@ export default class Popup extends React.Component<PopupProps> {
                 </div>
             </div>
         );
+    }
+
+    static show(popup: JSX.Element): void {
+        const target = document.getElementById('react-overlay-target');
+        ReactDOM.render(popup, target);
+        target?.classList.add('active');
     }
 }
