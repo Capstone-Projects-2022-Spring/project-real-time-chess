b6acdfdaa5a8ea81adb3226f97bb48eb8485862b
valeriekeo <valeriekeolam@gmail.com>
2022-03-13 17:32:40 -0400

    Added some comments how to implement AIvsPlayer gameplay

--- static/src/ts/components/ChessboardComponent.tsx
+++ static/src/ts/components/ChessboardComponent.tsx
@@ -32,7 +32,7 @@ export default class ChessboardComponent extends React.Component<
     AIplay() {
         if (!chess.gameOver()) {
             const moves = chess.moves()
-            const move:string = moves[Math.floor(Math.random() * moves.length)]!
+            const move: string = moves[Math.floor(Math.random() * moves.length)]!
             console.log(chess.move(move))
             console.log(chess.fen())
             this.setState({
@@ -41,14 +41,38 @@ export default class ChessboardComponent extends React.Component<
         }
     }
 
-    YouvsAIplay(){
-        
+    YouvAIplay(){
+        while(!chess.gameOver()){
+            //let the player make a move
+            //determine when the piece is dropped
+            //update the fen string
+            //call AIResponse
+        }
+    }
+
+    AIResponse(): void {
+        const moves = chess.moves()
+        const move: string = moves[Math.floor(Math.random() * moves.length)]!
+        console.log(chess.move(move))
+        console.log(chess.fen())
+        chess.move(move)
+        //update the fen string
+        this.setState({
+            position: chess.fen()
+        })
     }
 
     render() {
         return (
             <div>
                 {<Chessboard
+                    //arePiecesDraggable={true}
+                    //figure out how to make oNLY the players pieces draggable
+                    //consider assigning hte player a random color
+                    //^if you do that, change board orientation
+                    //could highlight all possible move positions onPieceClick
+                    //onPieceClick{()=>//callsomemethod}
+                    //onPieceDrop => update fen string
                     arePiecesDraggable={false}
                     animationDuration={200}
                     position={this.state.position}
@@ -56,6 +80,8 @@ export default class ChessboardComponent extends React.Component<
                 <ButtonComponent label='Make A Move' onClick={()=>{
                     if (this.state.mode === 'AIvAI')
                         this.AIplay()
+                    //if(this.state.mode === 'YouvAI')
+                    //  this.YouvAIplay()
                 }}/>
             </div>
           );
