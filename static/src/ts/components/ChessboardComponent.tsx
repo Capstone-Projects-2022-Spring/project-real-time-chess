5efb4cc9387111bcdc60d50690d77af8754205a5
Viraj Shah <shahvirajd@gmail.com>
2022-03-14 04:22:44 -0400

    Implemented move validator on frontend

--- static/src/ts/components/ChessboardComponent.tsx
+++ static/src/ts/components/ChessboardComponent.tsx
@@ -1,18 +1,50 @@
-// import { Chess } from 'chess.js';
+import { Chess, ChessInstance } from 'chess.js';
 import * as React from 'react';
 import { Chessboard } from 'react-chessboard';
-// const chess = new Chess();
+import Swal from 'sweetalert2';
 
-export default class ChessboardComponent extends React.Component<{}, {}> {
+export default class ChessboardComponent extends React.Component<
+    {},
+    {
+        game: ChessInstance;
+    }
+> {
     constructor(props: {}) {
         super(props);
+        try {
+            this.state = {
+                game: Chess(),
+            };
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     render() {
         return (
             <div>
-                <Chessboard />
+                <Chessboard
+                    position={this.state.game.fen()}
+                    onPieceDrop={(source, target) => {
+                        const isValid = this.tryMove(source, target);
+                        if (isValid) {
+                            this.forceUpdate();
+                        } else {
+                            Swal.fire({
+                                icon: 'warning',
+                                title: 'Invalid move',
+                                text: 'Please try again',
+                            });
+                        }
+                        return false;
+                    }}
+                />
             </div>
         );
+    }
+
+    private tryMove(source: string, target: string): boolean {
+        const move = this.state.game.move(`${source}-${target}`, { sloppy: true });
+        return move === null ? false : true;
     }
 }
