ac239acb6c6bfe5e94904215fbfe8ccd05b52789
Viraj Shah <shahvirajd@gmail.com>
2022-03-17 14:02:56 -0400

    Turned gameRoutes and userRoutes into a class

--- src/main/routes/gameRoutes.ts
+++ src/main/routes/gameRoutes.ts
@@ -7,127 +7,132 @@ import GameManager from '../GameManager';
 import GameMessagesAPIResponse from '../GameMessagesAPIResponse';
 
 /**
- * The handler for requests where a game is created
- *
- * @param req - The express request object
- * @param res - The express response object
+ * Utility class for all the game routes
  */
-function createGame(req: Request, res: Response) {
-    GameManager.verifyUserAccess(req.cookies.uid, req.cookies.auth)
-        .then(user => {
-            const game = GameManager.createGame(user);
-            if (game) {
-                res.send(new GameCreatedAPIResponse(game.gameKey));
-            } else {
-                res.send(new ErrorAPIResponse('User not found.'));
-            }
-        })
-        .catch(err => {
-            res.send(new ErrorAPIResponse(err));
-        });
-}
+class GameRoutes {
+    /**
+     * The handler for requests where a game is created
+     *
+     * @param req - The express request object
+     * @param res - The express response object
+     */
+    static createGame(req: Request, res: Response) {
+        GameManager.verifyUserAccess(req.cookies.uid, req.cookies.auth)
+            .then(user => {
+                const game = GameManager.createGame(user);
+                if (game) {
+                    res.send(new GameCreatedAPIResponse(game.gameKey));
+                } else {
+                    res.send(new ErrorAPIResponse('User not found.'));
+                }
+            })
+            .catch(err => {
+                res.send(new ErrorAPIResponse(err));
+            });
+    }
 
-/**
- * The handler for requests where a user joins a game.
- *
- * @param req - The express request object
- * @param res - The express response object
- */
-function joinGame(req: Request, res: Response) {
-    const uid = new ObjectId(req.cookies.uid);
-    GameManager.verifyUserAccess(req.cookies.uid, req.cookies.auth)
-        .then(user => {
-            const game = GameManager.findGameByUser(uid);
-            if (game) {
-                game.white = user;
-                game.addMessage({ message: `${user.name.first} joined the game.` });
-                res.send(new GameCreatedAPIResponse(game.gameKey));
-            } else {
-                res.send(new ErrorAPIResponse('Could not find game'));
-            }
-        })
-        .catch(() => {
-            res.send(new ErrorAPIResponse('Unknown Database Error'));
-        });
-}
-
-/**
- * The handler for requests where a user moves a chessboard piece
- *
- * @param req - The express request object
- * @param res - The express response object
- */
-function movePiece(req: Request, res: Response) {
-    const dao = new UserDAO();
-    const uid = new ObjectId(req.cookies.uid);
-    dao.authenticateKey(uid, req.cookies.auth)
-        .then(authorized => {
-            if (authorized) {
+    /**
+     * The handler for requests where a user joins a game.
+     *
+     * @param req - The express request object
+     * @param res - The express response object
+     */
+    static joinGame(req: Request, res: Response) {
+        const uid = new ObjectId(req.cookies.uid);
+        GameManager.verifyUserAccess(req.cookies.uid, req.cookies.auth)
+            .then(user => {
                 const game = GameManager.findGameByUser(uid);
                 if (game) {
-                    const { source, target } = req.body;
-                    const move = game.move(source, target);
-                    if (move) {
-                        res.send({ success: true, move });
+                    game.white = user;
+                    game.addMessage({ message: `${user.name.first} joined the game.` });
+                    res.send(new GameCreatedAPIResponse(game.gameKey));
+                } else {
+                    res.send(new ErrorAPIResponse('Could not find game'));
+                }
+            })
+            .catch(() => {
+                res.send(new ErrorAPIResponse('Unknown Database Error'));
+            });
+    }
+
+    /**
+     * The handler for requests where a user moves a chessboard piece
+     *
+     * @param req - The express request object
+     * @param res - The express response object
+     */
+    static movePiece(req: Request, res: Response) {
+        const dao = new UserDAO();
+        const uid = new ObjectId(req.cookies.uid);
+        dao.authenticateKey(uid, req.cookies.auth)
+            .then(authorized => {
+                if (authorized) {
+                    const game = GameManager.findGameByUser(uid);
+                    if (game) {
+                        const { source, target } = req.body;
+                        const move = game.move(source, target);
+                        if (move) {
+                            res.send({ success: true, move });
+                        } else {
+                            res.send(new ErrorAPIResponse('Invalid move'));
+                        }
                     } else {
-                        res.send(new ErrorAPIResponse('Invalid move'));
+                        res.send({
+                            success: false,
+                            error: new Error(`No game with user ${req.cookies.cookie}`),
+                        });
                     }
-                } else {
-                    res.send({
-                        success: false,
-                        error: new Error(`No game with user ${req.cookies.cookie}`),
-                    });
-                }
-            } else res.send({ success: false, error: new Error('Invalid User') });
-        })
-        .catch(err => {
-            res.send({ success: false, error: err });
-        });
-}
+                } else res.send({ success: false, error: new Error('Invalid User') });
+            })
+            .catch(err => {
+                res.send({ success: false, error: err });
+            });
+    }
 
-/**
- * The handler for when the client requests the Forsyth-Edwards Notation.
- * This is used by ChessJS to configure the pieces on the board.
- *
- * @param req - The express request object
- * @param res - The express response object
- */
-function getFEN(req: Request, res: Response) {
-    const dao = new UserDAO();
-    const uid = new ObjectId(req.cookies.uid);
-    dao.authenticateKey(uid, req.cookies.auth)
-        .then(authorized => {
-            if (authorized) {
-                const game = GameManager.findGameByUser(uid);
-                if (game) {
-                    res.send({ success: true, fen: game.fen });
-                } else {
-                    res.send({
-                        success: false,
-                        error: new Error(`No game with user ${uid}`),
-                    });
-                }
-            } else res.send({ success: false, error: new Error('Invalid User') });
-        })
-        .catch(err => {
-            res.send({ success: false, error: err });
-        });
-}
+    /**
+     * The handler for when the client requests the Forsyth-Edwards Notation.
+     * This is used by ChessJS to configure the pieces on the board.
+     *
+     * @param req - The express request object
+     * @param res - The express response object
+     */
+    static getFEN(req: Request, res: Response) {
+        const dao = new UserDAO();
+        const uid = new ObjectId(req.cookies.uid);
+        dao.authenticateKey(uid, req.cookies.auth)
+            .then(authorized => {
+                if (authorized) {
+                    const game = GameManager.findGameByUser(uid);
+                    if (game) {
+                        res.send({ success: true, fen: game.fen });
+                    } else {
+                        res.send({
+                            success: false,
+                            error: new Error(`No game with user ${uid}`),
+                        });
+                    }
+                } else res.send({ success: false, error: new Error('Invalid User') });
+            })
+            .catch(err => {
+                res.send({ success: false, error: err });
+            });
+    }
 
-/**
- * The handler for when the client requests the game messages.
- *
- * @param req - The express request object.
- * @param res - The express response object.
- */
-function getMessages(req: Request, res: Response) {
-    const uid = new ObjectId(req.cookies.uid);
-    const game = GameManager.findGameByUser(uid);
-    if (game) {
-        res.send(new GameMessagesAPIResponse(game.getMessages()));
-    } else {
-        res.send(new ErrorAPIResponse('Could not find game'));
+    /**
+     * The handler for when the client requests the game messages.
+     *
+     * @param req - The express request object.
+     * @param res - The express response object.
+     */
+    static getMessages(req: Request, res: Response) {
+        const uid = new ObjectId(req.cookies.uid);
+        const game = GameManager.findGameByUser(uid);
+        if (game) {
+            res.send(new GameMessagesAPIResponse(game.getMessages()));
+        } else {
+            res.send(new ErrorAPIResponse('Could not find game'));
+        }
     }
 }
 
-export { createGame, joinGame, movePiece, getFEN, getMessages };
+export default GameRoutes;
--- src/main/routes/userRoutes.ts
+++ src/main/routes/userRoutes.ts
@@ -14,64 +14,66 @@ interface CreateUserRequestBody {
     password: string;
 }
 
-/**
- * The handler for the user registration route.
- *
- * @param req - The Express request object.
- * @param res - The Express response object.
- */
-function createUserRoute(
-    req: express.Request<Record<string, never>, BaseAPIResponse, CreateUserRequestBody>,
-    res: express.Response<BaseAPIResponse>,
-) {
-    const dao = new UserDAO();
-    dao.createUser(req.body)
-        .then(() =>
-            res.send({
-                success: true,
-            }),
-        )
-        .catch(err => res.send(new ErrorAPIResponse(err)));
+class UserRoutes {
+    /**
+     * The handler for the user registration route.
+     *
+     * @param req - The Express request object.
+     * @param res - The Express response object.
+     */
+    static createUserRoute(
+        req: express.Request<Record<string, never>, BaseAPIResponse, CreateUserRequestBody>,
+        res: express.Response<BaseAPIResponse>,
+    ) {
+        const dao = new UserDAO();
+        dao.createUser(req.body)
+            .then(() =>
+                res.send({
+                    success: true,
+                }),
+            )
+            .catch(err => res.send(new ErrorAPIResponse(err)));
+    }
+
+    /**
+     * The handler for handling a login form request.
+     *
+     * @param req - The Express request object
+     * @param res - The Express response object
+     */
+    static loginUserRoute(
+        req: express.Request<
+            Record<string, never>,
+            LoginAPIResponse,
+            { user: string; password: string }
+        >,
+        res: express.Response<LoginAPIResponse | ErrorAPIResponse>,
+    ) {
+        const dao = new UserDAO();
+        dao.authenticateLogin(req.body)
+            .then(auth => {
+                res.cookie('uid', auth.uid);
+                res.cookie('auth', auth.key);
+                res.send(new LoginAPIResponse(auth));
+            })
+            .catch(err => res.send(new ErrorAPIResponse(err)));
+    }
+
+    /**
+     * The handler for handling a certificate authentication request.
+     *
+     * @param req - The Express request object.
+     * @param res - The Express response object.
+     */
+    static authenticateUserRoute(req: express.Request, res: express.Response<BaseAPIResponse>) {
+        const dao = new UserDAO();
+        dao.authenticateKey(new ObjectId(req.cookies.uid), req.cookies.auth)
+            .then(passed => {
+                if (passed) res.send({ success: true });
+                else res.send(new ErrorAPIResponse(new Error('Invalid certificate')));
+            })
+            .catch(error => res.send(new ErrorAPIResponse(error)));
+    }
 }
 
-/**
- * The handler for handling a login form request.
- *
- * @param req - The Express request object
- * @param res - The Express response object
- */
-function loginUserRoute(
-    req: express.Request<
-        Record<string, never>,
-        LoginAPIResponse,
-        { user: string; password: string }
-    >,
-    res: express.Response<LoginAPIResponse | ErrorAPIResponse>,
-) {
-    const dao = new UserDAO();
-    dao.authenticateLogin(req.body)
-        .then(auth => {
-            res.cookie('uid', auth.uid);
-            res.cookie('auth', auth.key);
-            res.send(new LoginAPIResponse(auth));
-        })
-        .catch(err => res.send(new ErrorAPIResponse(err)));
-}
-
-/**
- * The handler for handling a certificate authentication request.
- *
- * @param req - The Express request object.
- * @param res - The Express response object.
- */
-function authenticateUserRoute(req: express.Request, res: express.Response<BaseAPIResponse>) {
-    const dao = new UserDAO();
-    dao.authenticateKey(new ObjectId(req.cookies.uid), req.cookies.auth)
-        .then(passed => {
-            if (passed) res.send({ success: true });
-            else res.send(new ErrorAPIResponse(new Error('Invalid certificate')));
-        })
-        .catch(error => res.send(new ErrorAPIResponse(error)));
-}
-
-export { createUserRoute, loginUserRoute, authenticateUserRoute };
+export default UserRoutes;
