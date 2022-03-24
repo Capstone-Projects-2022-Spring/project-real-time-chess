65d920c1954af7479e966c37662df50f5ee0e9f8
Viraj Shah <shahvirajd@gmail.com>
2022-03-24 19:35:52 -0400

    Removed GameAccess methods which now use WebSockets

--- static/src/ts/access/GameAccess.ts
+++ static/src/ts/access/GameAccess.ts
@@ -1,18 +1,6 @@
 import axios from 'axios';
-import { Square } from 'chess.js';
 
 class GameAccess {
-    static getGameState(): Promise<IGameStateAPIResponse> {
-        return new Promise((resolve, reject) => {
-            axios
-                .get('/api/game')
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(err => reject(err));
-        });
-    }
-
     static async createGame(): Promise<IGameCreatedAPIResponse> {
         return new Promise((resolve, reject) => {
             axios
@@ -27,51 +15,6 @@ class GameAccess {
             axios
                 .post('/api/game/join', { gameKey })
                 .then(response => resolve(response.data))
-                .catch(err => reject(err));
-        });
-    }
-
-    static async move(source: Square, target: Square): Promise<APIResponse> {
-        return new Promise((resolve, reject) => {
-            axios
-                .post('/api/game/move', { source, target })
-                .then(response => {
-                    if (response.data.success) {
-                        resolve(response.data);
-                    } else {
-                        reject(response.data.error);
-                    }
-                })
-                .catch(err => reject(err));
-        });
-    }
-
-    static async getMessages(): Promise<IGameMessage[]> {
-        return new Promise((resolve, reject) => {
-            axios
-                .get('/api/game/messages')
-                .then(response => {
-                    if (response.data.success) {
-                        resolve(response.data.messages);
-                    } else {
-                        reject(response.data.error);
-                    }
-                })
-                .catch(err => reject(err));
-        });
-    }
-
-    static async getFEN(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            axios
-                .get('/api/game/fen')
-                .then(response => {
-                    if (response.data.success) {
-                        resolve(response.data.fen);
-                    } else {
-                        reject(response.data.error);
-                    }
-                })
                 .catch(err => reject(err));
         });
     }
