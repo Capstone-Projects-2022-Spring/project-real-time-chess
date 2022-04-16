9f4e2c3850570a85aee48bba39239b75acbba7f8
Viraj Shah <shahvirajd@gmail.com>
2022-04-16 15:01:28 -0400

    Modified logger for cleaner test output
    Before: Printed to console AND server.log
    Now: It now only prints to server.test.log when testing.

--- src/main/Logger.ts
+++ src/main/Logger.ts
@@ -1,11 +1,49 @@
 import * as winston from 'winston';
 
+const RTCLogFormat = winston.format.printf(
+    ({ level, message, label, timestamp }) => `${timestamp} [${label}] ${level}: ${message}`,
+);
+
+const transports = [];
+
+if (process.env.NODE_ENV === 'test') {
+    transports.push(
+        new winston.transports.File({
+            filename: 'server.test.log',
+            format: winston.format.combine(
+                winston.format.label({ label: 'RTC-Server' }),
+                winston.format.timestamp(),
+                winston.format.prettyPrint(),
+                winston.format.colorize(),
+                RTCLogFormat,
+            ),
+        }),
+    );
+} else {
+    transports.push(
+        new winston.transports.File({
+            filename: 'server.log',
+            format: winston.format.combine(
+                winston.format.label({ label: 'RTC-Server' }),
+                winston.format.timestamp(),
+                winston.format.prettyPrint(),
+                winston.format.colorize(),
+                RTCLogFormat,
+            ),
+        }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.label({ label: 'RTC-Server' }),
+                winston.format.timestamp(),
+                RTCLogFormat,
+            ),
+        }),
+    );
+}
+
 const Logger = winston.createLogger({
     level: 'silly',
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'server.log' }),
-    ],
+    transports,
 });
 
 export default Logger;
