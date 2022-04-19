3498a02e5dcf8320986def57946e577e797de834
Viraj Shah <shahvirajd@gmail.com>
2022-04-19 03:27:48 -0400

    Simplified toast notifications

--- static/src/ts/components/UI/ToastNotification.ts
+++ static/src/ts/components/UI/ToastNotification.ts
@@ -1,5 +1,7 @@
 import Toastify from 'toastify-js';
 
+type ToastGravity = 'top' | 'bottom';
+
 /**
  * A class for displaying toast notifications.
  */
@@ -12,21 +14,13 @@ class ToastNotification {
      * @param text - The text to display in the toast notification.
      * @param duration - The length of time (in milliseconds) to display the toast notification.
      * @param gravity - Whether the toast should be displayed starting from the top or the bottom.
-     * @param style - Additional styles to apply.
      * @param onClick - Behavior to execute when the toast notification is clicked.
      */
-    constructor(
-        text: string,
-        duration: number,
-        gravity?: 'top' | 'bottom',
-        style?: { [rule: string]: string },
-        onClick?: () => void,
-    ) {
+    constructor(text: string, duration: number, gravity?: ToastGravity, onClick?: () => void) {
         this.toastifyOptions = {
             text,
             duration,
             gravity: gravity ?? 'top',
-            style,
             onClick,
             position: 'right',
             close: true,
@@ -47,18 +41,16 @@ class ToastNotification {
      * @param text - The text to display in the toast notification.
      * @param duration - The length of time (in milliseconds) to display the toast notification.
      * @param gravity - Whether the toast should be displayed starting from the top or the bottom.
-     * @param style - Additional styles to apply.
      * @param onClick - Behavior to execute when the toast notification is clicked.
      * @returns The toast notification object that was used to notify the user.
      */
     static notify(
         text: string,
         duration: number,
-        gravity?: 'top' | 'bottom',
-        style?: { [rule: string]: string },
+        gravity?: ToastGravity,
         onClick?: () => void,
     ): ToastNotification {
-        const obj = new ToastNotification(text, duration, gravity, style, onClick);
+        const obj = new ToastNotification(text, duration, gravity, onClick);
         obj.fire();
         return obj;
     }
