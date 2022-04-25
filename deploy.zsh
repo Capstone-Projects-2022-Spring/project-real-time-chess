1747f5e624a9314944511b63b8d13dbb01461060
Viraj Shah <shahvirajd@gmail.com>
2022-04-24 20:16:10 -0400

    Changed deploy command

--- deploy.zsh
+++ deploy.zsh
@@ -4,7 +4,8 @@ mkdir deployments
 cp -r dist deployments
 mkdir deployments/static
 mkdir deployments/static/public
-cp -r static/public deployments/static
+cp -r static/public deployments/static/public
 cp -r views deployments
-cp package.prod.json deployments/package.json
+cp -r package.json deployments
+cp -r package-lock.json deployments
 cp -r Procfile deployments
